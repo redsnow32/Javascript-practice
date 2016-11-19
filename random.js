5912d5185969271677d75bb216fc3ceaba645ea7
brandon allred <b32alls@gmail.com>
2016-11-18 18:10:19 -0700

    else if statements

--- random.js
+++ random.js
@@ -42,3 +42,28 @@ else if (number==4)
   {
     return 8;
   }
+
+  --------------------------------------------------------------------------------
+
+
+function testSize(num) {
+  // Only change code below this line
+  if (num < 5 ) {
+    return "Tiny";
+  }
+  else if (num < 10 ) {
+    return "Small";
+  }
+
+  else if (num < 15 ) {
+    return "Medium";
+  }
+  else if (num < 20 ) {
+    return "Large";
+  }
+  else if (num >= 25) {
+
+  }
+  return "Huge";
+}
+testSize(7);
