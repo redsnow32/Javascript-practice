1e0d18132c409e9d15f0e66ae540bae0d8ab4440
brandon allred <b32alls@gmail.com>
2016-11-20 20:52:48 -0700

    golfers problem

--- random.js
+++ random.js
@@ -67,3 +67,39 @@ function testSize(num) {
   return "Huge";
 }
 testSize(7);
+
+---------------------------------------------------------------------------------------
+
+
+function golfScore(par, strokes) {
+  // Only change code below this line
+  if (strokes ==1) {
+    return "Hole-in-one!";
+  }
+  else if (strokes <= par - 2) {
+    return "Eagle";
+
+    }
+  else if (strokes <= par - 1) {
+    return "Birdie";
+
+    }
+  else if (par == strokes) {
+
+  return "Par";
+  }
+
+  else if (strokes == par + 1)
+  {
+    return "Bogey";
+  }
+
+  else if (strokes == par + 2) {
+
+  return "Double Bogey";
+  }
+  else if (strokes >= par +3) {
+    return "Go Home!";
+  }
+}
+golfScore(4, 9);
