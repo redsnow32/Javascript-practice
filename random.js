d5c54f1efdcaa23029a3cfca5766e5a6a2036caf
brandon allred <b32alls@gmail.com>
2016-11-21 16:18:32 -0700

    switch function

--- random.js
+++ random.js
@@ -103,3 +103,29 @@ function golfScore(par, strokes) {
   }
 }
 golfScore(4, 9);
+
+----------------------------------------------------------------------------------------------------------------------
+function caseInSwitch(val) {
+  var answer = "";
+  // Only change code below this line
+  switch (val) {
+    case 1:
+      answer = "alpha";
+      break;
+
+    case 2:
+      answer = "beta";
+      break;
+
+    case 3:
+      answer = "gamma";
+      break;
+
+    case 4:
+      answer = "delta";
+      break;
+  }
+   
+}
+
+caseInSwitch(1);
