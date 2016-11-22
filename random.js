48c24fe12c97bd2c8835371a6c0d1c34b90be110
brandon allred <b32alls@gmail.com>
2016-11-22 08:33:51 -0700

    more practice

--- random.js
+++ random.js
@@ -212,3 +212,20 @@ function chainToSwitch(val) {
 }
 
 chainToSwitch(7);
+
+---------------------------------------------------------------------------------------------------------------------
+function abTest(a, b) {
+
+  // Only change code below this line
+  if ( a < 0 || b < 0){
+  return undefined;
+
+} else {
+
+  // Only change code above this line
+
+  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
+}
+// Change values below to test your code
+abTest(-2,2);
