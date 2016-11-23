bc95a60b55803707f11b4cdd8db3e4da3742fd2b
brandon allred <b32alls@gmail.com>
2016-11-22 22:49:21 -0700

    Counting Cards Problem

--- random.js
+++ random.js
@@ -229,3 +229,33 @@ function abTest(a, b) {
 }
 // Change values below to test your code
 abTest(-2,2);
+
+---------------------------------------------------------------------------------------------------------------------
+function cc(card) {
+   switch (card){
+     case 2:
+     case 3:
+     case 4:
+     case 5:
+     case 6:
+       count ++;
+       break;
+
+     case 10:
+     case "J":
+     case "Q":
+      case"K":
+     case "A":
+       count --;
+       break;
+   }
+  if(count>0){
+    return count+" Bet";
+  }
+  else {
+    return count+" Hold";
+  }
+
+  ---------------------------------------------------------------------------------------------------------------------
+
+  
