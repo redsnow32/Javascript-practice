a4c402b4fb140a487cfc73f696e21b4d45476c69
brandon allred <b32alls@gmail.com>
2016-11-24 07:20:37 -0800

    Third way to do counting cards problem

--- random.js
+++ random.js
@@ -269,3 +269,35 @@ function cc(card) {
 
   return count + ((count > 0) ? ' Bet':' Hold');
 }
+
+--------------------------------------------------------------------------------------------------------------------
+var count = 0;
+
+function cc(card) {
+  // Only change code below this line
+
+  if(card >= 2 && card <= 6){
+    count++;
+     if(count<=0){ return (count+" Hold"); } else{ return (count+" Bet"); } } else if(card >= 7 && card <= 9) {
+
+     if(count<=0){
+    return (count+" Hold");
+     }
+
+    else{
+      return (count+" Bet");
+    }
+  }
+
+   else if(card===10 || card==="J" ||  card==="Q" || card==="K" || card==="A"){
+    count--;
+    if(count<=0){
+
+    return (count+" Hold");
+     }
+
+    else{
+      return (count+" Bet");
+    }
+  }
+}
