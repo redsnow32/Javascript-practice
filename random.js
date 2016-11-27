2527e083976c9a343dff18edff5a65b2eb220d73
brandon allred <b32alls@gmail.com>
2016-11-27 16:23:28 -0700

    accessing objects

--- random.js
+++ random.js
@@ -324,3 +324,15 @@ var shirtValue = testObj.shirt;
 
 
 --------------------------------------------------------------------------------------------------------------------
+
+var testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+
+var entreeValue = testObj ["an entree"];
+var drinkValue = testObj ["the drink"];
+
+--------------------------------------------------------------------------------------------------------------------
