9801da564cd695c6200c06c5db7c1e2f9b9bd12f
brandon allred <b32alls@gmail.com>
2016-11-28 17:02:46 -0700

    updating object properties

--- random.js
+++ random.js
@@ -336,3 +336,27 @@ var entreeValue = testObj ["an entree"];
 var drinkValue = testObj ["the drink"];
 
 --------------------------------------------------------------------------------------------------------------------
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+ourDog.name = "Happy Camper";
+
+// Setup
+var myDog = {
+  "name": "Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["Free Code Camp Campers"]
+};
+
+// Only change code below this line.
+
+myDog.name = "Happy Coder";
+
+--------------------------------------------------------------------------------------------------------------------
