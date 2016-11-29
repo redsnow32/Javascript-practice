8b3e46a243539411a351afa864c83ff89b450d14
brandon allred <b32alls@gmail.com>
2016-11-28 17:05:16 -0700

    adding new properties to object

--- random.js
+++ random.js
@@ -360,3 +360,26 @@ var myDog = {
 myDog.name = "Happy Coder";
 
 --------------------------------------------------------------------------------------------------------------------
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+ourDog.bark = "bow-wow";
+
+// Setup
+var myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["Free Code Camp Campers"]
+};
+
+// Only change code below this line.
+ myDog.bark = "woof";
+
+ --------------------------------------------------------------------------------------------------------------------
