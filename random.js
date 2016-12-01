4e83a0bea5ec3931086c0a20b6c4d8775b788626
brandon allred <b32alls@gmail.com>
2016-11-30 23:04:34 -0700

    problems

--- random.js
+++ random.js
@@ -393,3 +393,36 @@ var myDog = {
  };
 
  delete myDog.tails;
+
+ --------------------------------------------------------------------------------------------------------------------
+
+ function phoneticLookup(val) {
+   var result = "";
+
+   // Only change code below this line
+   switch(val) {
+     case "alpha":
+       result = "Adams";
+       break;
+     case "bravo":
+       result = "Boston";
+       break;
+     case "charlie":
+       result = "Chicago";
+       break;
+     case "delta":
+       result = "Denver";
+       break;
+     case "echo":
+       result = "Easy";
+       break;
+     case "foxtrot":
+       result = "Frank";
+   }
+
+   // Only change code above this line
+   return result;
+ }
+
+ // Change this value to test
+ phoneticLookup("charlie");
