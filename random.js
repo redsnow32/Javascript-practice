34be0bd173623989bad3678b848daefff5e950ea
brandon allred <b32alls@gmail.com>
2016-12-01 21:08:46 -0700

    fixed problem

--- random.js
+++ random.js
@@ -396,33 +396,23 @@ var myDog = {
 
  --------------------------------------------------------------------------------------------------------------------
 
+
  function phoneticLookup(val) {
    var result = "";
 
-   // Only change code below this line
-   switch(val) {
-     case "alpha":
-       result = "Adams";
-       break;
-     case "bravo":
-       result = "Boston";
-       break;
-     case "charlie":
-       result = "Chicago";
-       break;
-     case "delta":
-       result = "Denver";
-       break;
-     case "echo":
-       result = "Easy";
-       break;
-     case "foxtrot":
-       result = "Frank";
-   }
+   var lookup = {
+      "alpha": "Adams",
+     "bravo": "Boston",
+     "charlie": "Chicago",
+     "delta":"Denver",
+     "echo":"Easy",
+     "foxtrot":"Frank",
 
-   // Only change code above this line
+   };
+   result = lookup [val];
+
    return result;
  }
 
- // Change this value to test
  phoneticLookup("charlie");
+ --------------------------------------------------------------------------------------------------------------------
