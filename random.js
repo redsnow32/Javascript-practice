db6ce060dcce4d53cf8f80b7aa2a5a19b5e4e0fd
brandon allred <b32alls@gmail.com>
2016-12-27 14:51:24 -0700

    objects with constructor functions

--- random.js
+++ random.js
@@ -715,3 +715,13 @@ var MotorBike = function() {
 };
 
 --------------------------------------------------------------------------------------------------------------------
+var Car = function() {
+  this.wheels = 4;
+  this.engines = 1;
+  this.seats = 5;
+};
+
+
+var myCar = new Car();
+myCar.wheels = 4;
+myCar.nickname = "turbo";
